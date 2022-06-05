346d42dafdbcf077a0628333605b3362e9571cf6
ruan <ruantiengo@gmail.com>
2022-06-05 12:01:17 -0300

    chore: api local

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Widget } from './components/WidgetForm/widget-button'
 
 function App () {
+  console.log(import.meta.env.VITE_API_URL)
+
   return (
     <div>
       <Widget></Widget>
